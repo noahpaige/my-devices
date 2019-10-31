1dc42edee357260fee232655843e7afc52edda51
Noah Paige <noahlandonpaige@gmail.com>
2019-10-31 11:25:02 -0700

    removed some dummy data

--- src/App.jsx
+++ src/App.jsx
@@ -8,84 +8,14 @@ class App extends Component {
     myDevices: [
       {
         id: 0,
-        name: "iPhone XR 1",
+        name: "iPhone XR",
         image:
           "https://d3nevzfk7ii3be.cloudfront.net/igi/yiiosKwJCvSrNS12.thumbnail",
         wikiLink: "https://www.ifixit.com/Device/iPhone_XR"
       },
       {
         id: 1,
-        name: "Xbox One 1",
-        image:
-          "https://d3nevzfk7ii3be.cloudfront.net/igi/GtHYwOXhf6TNnp2h.large",
-        wikiLink: "https://www.ifixit.com/Device/Xbox_One"
-      },
-      {
-        id: 2,
-        name: "iPhone XR 2",
-        image:
-          "https://d3nevzfk7ii3be.cloudfront.net/igi/yiiosKwJCvSrNS12.large",
-        wikiLink: "https://www.ifixit.com/Device/iPhone_XR"
-      },
-      {
-        id: 3,
-        name: "Xbox One 2",
-        image:
-          "https://d3nevzfk7ii3be.cloudfront.net/igi/GtHYwOXhf6TNnp2h.large",
-        wikiLink: "https://www.ifixit.com/Device/Xbox_One"
-      },
-      {
-        id: 4,
-        name: "iPhone XR 3",
-        image:
-          "https://d3nevzfk7ii3be.cloudfront.net/igi/yiiosKwJCvSrNS12.large",
-        wikiLink: "https://www.ifixit.com/Device/iPhone_XR"
-      },
-      {
-        id: 5,
-        name: "Xbox One 3",
-        image:
-          "https://d3nevzfk7ii3be.cloudfront.net/igi/GtHYwOXhf6TNnp2h.large",
-        wikiLink: "https://www.ifixit.com/Device/Xbox_One"
-      },
-      {
-        id: 6,
-        name: "iPhone XR 4",
-        image:
-          "https://d3nevzfk7ii3be.cloudfront.net/igi/yiiosKwJCvSrNS12.large",
-        wikiLink: "https://www.ifixit.com/Device/iPhone_XR"
-      },
-      {
-        id: 7,
-        name: "Xbox One 4",
-        image:
-          "https://d3nevzfk7ii3be.cloudfront.net/igi/GtHYwOXhf6TNnp2h.large",
-        wikiLink: "https://www.ifixit.com/Device/Xbox_One"
-      },
-      {
-        id: 8,
-        name: "iPhone XR 5",
-        image:
-          "https://d3nevzfk7ii3be.cloudfront.net/igi/yiiosKwJCvSrNS12.large",
-        wikiLink: "https://www.ifixit.com/Device/iPhone_XR"
-      },
-      {
-        id: 9,
-        name: "Xbox One 5",
-        image:
-          "https://d3nevzfk7ii3be.cloudfront.net/igi/GtHYwOXhf6TNnp2h.large",
-        wikiLink: "https://www.ifixit.com/Device/Xbox_One"
-      },
-      {
-        id: 10,
-        name: "iPhone XR 6",
-        image:
-          "https://d3nevzfk7ii3be.cloudfront.net/igi/yiiosKwJCvSrNS12.large",
-        wikiLink: "https://www.ifixit.com/Device/iPhone_XR"
-      },
-      {
-        id: 11,
-        name: "Xbox One 6",
+        name: "Xbox One",
         image:
           "https://d3nevzfk7ii3be.cloudfront.net/igi/GtHYwOXhf6TNnp2h.large",
         wikiLink: "https://www.ifixit.com/Device/Xbox_One"
